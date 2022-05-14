46343a2663b1e373edb88c2dbd9f7e123e926471
Yixing-one <y3536zha@uwaterloo.ca>
2022-05-14 17:03:59 -0400

    initial commit

---
+++ first_react_project1/src/App.js
@@ -0,0 +1,32 @@
+import React from "react"
+import TodoList from './TodoList'
+import { useState, useRef} from 'react'
+import uuidv4 from 'uuid/v4'
+
+
+function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input ref={todoNameRef} type ="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button>Clear Complete</button>
+      <div>0 left to do</div>
+    </>
+  )
+}
+
+export default App;
